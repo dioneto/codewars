7f6252311544a48aee6d9a8276dff7cbb1308142
Carlos de Dio Neto <dedio.carlos@gmail.com>
2020-09-04 01:43:17 -0300

    Add big numbers

---
+++ dev/Katas/4AddingBigNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codewars.dev.Katas
+{
+    public static class AddingBigNumbers
+    {
+        public static string Add(string a, string b)
+        {
+            int[] numA = new int[a.Length];
+            int[] numB = new int[b.Length];
+
+            int[] ret = new int[a.Length > b.Length ? a.Length + 1 : b.Length + 1];
+
+            numA = a.Select(x => Convert.ToInt32(x)).ToArray();
+            numB = b.Select(x => Convert.ToInt32(x)).ToArray();
+
+            for (int i = 0; i < (a.Length > b.Length ? a.Length: b.Length); i++)
+            {
+                ret[ret.Length - i - 1] = a[a.Length - i - 1] + b[b.Length - i - 1];
+            }
+
+            return "0"; // Fix this!
+        }
+    }
+}
---
+++ test/codewars.test/AddingBigNumbersTest.cs
@@ -0,0 +1,42 @@
+﻿using codewars.dev.Katas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars.test
+{
+    class AddingBigNumbersTest
+    {
+        /*
+         * In this example you have to validate if a user input string is alphanumeric. 
+         * The given string is not nil/null/NULL/None, so you don't have to check that.
+         The string has the following conditions to be alphanumeric:
+            - At least one character ("" is not valid)
+            - Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+            - No whitespaces / underscore
+        */
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void Test_0()
+        {
+            Assert.AreEqual("444", AddingBigNumbers.Add("123", "321"));
+        }
+        [Test]
+        public void Test_1()
+        {
+            Assert.AreEqual("110", AddingBigNumbers.Add("11", "99"));
+        }
+        [Test]
+        public void Test_2()
+        {
+            Assert.AreEqual("3333333333222", AddingBigNumbers.Add("1111111111222", "2222222222222"));
+        }
+
+
+    }
+}
