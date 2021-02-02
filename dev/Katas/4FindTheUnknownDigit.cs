feade4c9a394096b2552712f34dc3ba6cb222bb7
Carlos de Dio Neto <dedio.carlos@gmail.com>
2021-02-01 23:17:12 -0300

    FindTheUnknownDigit

---
+++ dev/Katas/4FindTheUnknownDigit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace codewars.dev.Katas
+{
+    public static class FindTheUnknownDigit
+    {
+        public static int solveExpression(string value)
+        {
+            //"1+1=?"
+            var x = value.Split('=');
+            var dt = new DataTable();
+            int a, b;
+            for (int i = 9; i >= -9; i--)
+            {
+                a = (int)dt.Compute(x[0].Replace("?", i.ToString()), "");
+                b = Convert.ToInt32(x[1].Replace("?", i.ToString()));
+                if (a == b) return i;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ test/codewars.test/FindTheUnknownDigitTest.cs
@@ -0,0 +1,28 @@
+﻿using codewars.dev.Katas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars.test
+{
+    class FindTheUnknownDigitTest
+    {
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[TestCase("1+1=?", 2)]
+		[TestCase("123*45?=5?088", 6)]
+		[TestCase("-5?*-1=5?", 0)]
+		[TestCase("19--45=5?", -1)]
+		[TestCase("??*??=302?", 5)]
+		[TestCase("?*11=??", 2)]
+		[TestCase("??+??=??", -1)]
+		public void TestFindTheUnknownDigit(string value, int expected)
+		{
+			Assert.AreEqual(expected, FindTheUnknownDigit.solveExpression(value), string.Format("Input {0}", value));
+		}
+	}
+}
